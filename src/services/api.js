9533a3d87c28090a3f9d9c95fc1df93ca43d878f
RAJ-980 <shahiraj693@gmail.com>
2026-07-05 17:52:09 +0530

    Update API base URL to include '/api' path

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://quicknotes-backend-xh4w.onrender.com/"
+  baseURL: "https://quicknotes-backend-xh4w.onrender.com/api"
 });
 
 export default api;
